74777c5adba06b44913c1a6ca19bb4a5fe669e27
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-06-17 11:31:23 +0700

    Terraform template added

--- windowstest.tf
+++ windowstest.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "Ansible-Windows" {
   ami           = "ami-a94342d5"
   instance_type = "t2.micro"
-  security_groups = "sg-05043d7c"
+  security_groups = ["sg-05043d7c"]
   subnet_id = "subnet-be32b4f7"
   tags {
     Name = "Anisble-Windows"
