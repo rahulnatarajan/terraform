c053f2dc7dd8752ebbb7e13c09f3b3f41ca005d3
rahulnatarajan <rahul_natarajan@hotmail.com>
2018-06-25 18:25:18 +0700

    Updated TF template

--- wp-dev.tf
+++ wp-dev.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "wp-dev" {
   ami           = "ami-de90a5a2"
   instance_type = "t2.micro"
   security_groups = ["sg-7e5eb406"]
+  key_name = "awsfs_sg_kp"
   subnet_id = "subnet-06192061"
   tags {
     Name = "wp-dev"
